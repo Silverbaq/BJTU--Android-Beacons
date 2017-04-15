f7de291f7a67f18d7cd9abe9069e895214be8bc6
Steffen Mogensen <feber.sm@gmail.com>
2017-04-15 10:30:08 +0800

    Moved backend logic to a service

---
+++ app/src/main/java/dk/im2b/bjtu__android_beacons/BeaconDetectorService.java
@@ -0,0 +1,202 @@
+package dk.im2b.bjtu__android_beacons;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.List;
+
+public class BeaconDetectorService extends Service implements LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    // Location
+    Location mCurrentLocation;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    private static final long INTERVAL = 1000 * 10;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+
+    // Beacon
+    private BeaconManager beaconManager;
+    private Region region;
+
+    private String TAG = "BeaconDetectorService";
+
+
+    public BeaconDetectorService() {
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        // Location Listener
+        createLocationRequest();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mGoogleApiClient.connect();
+
+        //  Listening for beacons
+        beaconManager = new BeaconManager(getApplicationContext());
+        region = new Region("ranged region", null, null, null);
+
+        // iBeacon
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                // Check if beacon is in list (have been registered)
+                Log.d(TAG, "Checking Database");
+                for (Beacon b : list) {
+                    Log.d(TAG, "beacon Nearby!!!! : " + b);
+                    checkDatabase(b);
+                }
+            }
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+
+
+
+        Log.d(TAG, "Service started");
+
+        // Let it continue running until it is stopped.
+        //Toast.makeText(this, "Location Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        beaconManager.stopRanging(region);
+        beaconManager.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location.getAccuracy() < 20) {
+            mCurrentLocation = location;
+        }
+    }
+
+    private void checkDatabase(Beacon b) {
+        // checks if a Beacon is pressent in the database, if not it will add it
+        MyBeacon beacon = null;
+        try {
+            beacon = MyBeacon.find(MyBeacon.class, "uuid = ? and minor = ? and major = ?", b.getProximityUUID().toString(), "" + b.getMinor(), "" + b.getMajor()).get(0);
+            //Toast.makeText(this, ""+beacon, Toast.LENGTH_SHORT).show();
+        } catch (Exception ex) {
+            beacon = null;
+        }
+        if (mCurrentLocation != null) {
+            //Toast.makeText(this, "Is null", Toast.LENGTH_SHORT).show();
+            if (beacon == null) {
+                beacon = new MyBeacon(b.getProximityUUID().toString(), b.getMajor(), b.getMinor(), b.getRssi(), b.getMeasuredPower(), mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                beacon.save();
+
+            } else { // If the beacon is pressent - check if we are closer now and update the entry if nessaserry
+                double myDistance = calcDistance(beacon.getRssi(), beacon.getMeasuredPower());
+                double newDistance = calcDistance(b.getRssi(), b.getMeasuredPower());
+
+                // if we are closer, update the entry in the database and on the map
+                if (newDistance < myDistance) {
+                    beacon.setMeasuredPower(b.getMeasuredPower());
+                    beacon.setRssi(b.getRssi());
+                    beacon.setLatitude(mCurrentLocation.getLatitude());
+                    beacon.setLongitude(mCurrentLocation.getLongitude());
+                    beacon.save();
+                   // Toast.makeText(this, "works!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+            // Send broadcast that will update UI Map
+            sendBroadcast();
+        }
+    }
+
+    private double calcDistance(int rssi, int txPower) {
+    /*
+     * RSSI = TxPower - 10 * n * lg(d)
+     * n = 2 (in free space)
+     *
+     * d = 10 ^ ((TxPower - RSSI) / (10 * n))
+     */
+
+        return Math.pow(10d, ((double) txPower - rssi) / (10 * 2));
+    }
+
+    private void sendBroadcast(){
+        // local broadcast - information about activity for view
+        Intent intentForBroadcast = new Intent("MapUpdate");
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intentForBroadcast);
+
+    }
+
+}
--- app/src/main/java/dk/im2b/bjtu__android_beacons/MainActivity.java
+++ app/src/main/java/dk/im2b/bjtu__android_beacons/MainActivity.java
@@ -1,12 +1,16 @@
 package dk.im2b.bjtu__android_beacons;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -40,41 +44,25 @@ import com.orm.query.Select;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     // Map
     private GoogleMap mMap;
-    Location mCurrentLocation;
-    LocationRequest mLocationRequest;
-    GoogleApiClient mGoogleApiClient;
-    private static final long INTERVAL = 1000 * 10;
-    private static final long FASTEST_INTERVAL = 1000 * 5;
-
 
     // Beacon
     private BeaconManager beaconManager;
     private Region region;
 
-
     private String TAG = "MainActivity";
 
     ListView lvBeacon;
     BeaconDetailsAdapter beaconAdapter;
-
-    protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
+    private Intent beaconDetectorServiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // Make first beacon for the database
         MyBeacon beacon = new MyBeacon("FIRST", 0, 0, 0, 0, 0, 0);
@@ -88,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        createLocationRequest();
+
 
         // Beacon ListView
         lvBeacon = (ListView) findViewById(R.id.activityMain_ListviewBeacon);
@@ -103,71 +91,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         beaconManager.setRangingListener(new BeaconManager.RangingListener() {
             @Override
             public void onBeaconsDiscovered(Region region, List<Beacon> list) {
-                // Check if beacon is in list (have been registered)
-                for (Beacon b : list) {
-                    Log.d(TAG, "beacon Nearby!!!! : " + b);
-                    checkDatabase(b);
-                }
                 // update beacon adapter
                 beaconAdapter.updateItems(list);
-
-
-
             }
         });
-
         beaconManager.startRanging(region);
 
-        // Location Listener
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
+        // Start Service
+        beaconDetectorServiceIntent = new Intent(getBaseContext(), BeaconDetectorService.class);
+        startService(beaconDetectorServiceIntent);
 
-        mGoogleApiClient.connect();
+        // Registering local broadcast
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("MapUpdate"));
+
     }
 
-    private void checkDatabase(Beacon b) {
-        // checks if a Beacon is pressent in the database, if not it will add it
-        MyBeacon beacon = null;
-        try {
-/*
-            beacon = Select.from(MyBeacon.class)
-                    .where(Condition.prop("proximityUUID").eq(b.getProximityUUID().toString()),
-                            Condition.prop("minor").eq(b.getMinor()),
-                            Condition.prop("major").eq(b.getMajor()))
-                    .first();
-*/
-            beacon = MyBeacon.find(MyBeacon.class, "uuid = ? and minor = ? and major = ?", b.getProximityUUID().toString(), "" + b.getMinor(), "" + b.getMajor()).get(0);
-            //Toast.makeText(this, ""+beacon, Toast.LENGTH_SHORT).show();
-        } catch (Exception ex) {
-            beacon = null;
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateBeaconsOnMap();
         }
-        if (mCurrentLocation != null) {
+    };
 
-            if (beacon == null) {
-                beacon = new MyBeacon(b.getProximityUUID().toString(), b.getMajor(), b.getMinor(), b.getRssi(), b.getMeasuredPower(), mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
-                beacon.save();
 
-            } else { // If the beacon is pressent - check if we are closer now and update the entry if nessaserry
-                double myDistance = calcDistance(beacon.getRssi(), beacon.getMeasuredPower());
-                double newDistance = calcDistance(b.getRssi(), b.getMeasuredPower());
-
-                // if we are closer, update the entry in the database and on the map
-                if (newDistance < myDistance) {
-                    beacon.setMeasuredPower(b.getMeasuredPower());
-                    beacon.setRssi(b.getRssi());
-                    beacon.setLatitude(mCurrentLocation.getLatitude());
-                    beacon.setLongitude(mCurrentLocation.getLongitude());
-                    beacon.save();
-                }
-            }
-        }
-        addAndUpdateBeaconsOnMap();
-    }
-
-    private void addAndUpdateBeaconsOnMap() {
+    private void updateBeaconsOnMap() {
         mMap.clear();
 
         List<MyBeacon> beacons = MyBeacon.listAll(MyBeacon.class);
@@ -202,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             return;
         }
         mMap.setMyLocationEnabled(true);
-        addAndUpdateBeaconsOnMap();
+        updateBeaconsOnMap();
     }
 
     @Override
@@ -229,44 +177,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onDestroy() {
         super.onDestroy();
         beaconManager.disconnect();
+        stopService(beaconDetectorServiceIntent);
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient, mLocationRequest, this);
-    }
 
-    @Override
-    public void onConnectionSuspended(int i) {
 
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (location.getAccuracy() < 20) {
-            mCurrentLocation = location;
-
-            double latitude = location.getLatitude();
-            double longitude = location.getLongitude();
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude, longitude)));
-        }
-    }
 
     private double calcDistance(int rssi, int txPower) {
     /*
@@ -300,4 +215,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return super.onOptionsItemSelected(item);
         }
     }
+
 }
